0505d5009117a644a2a83390ec5ce087137515e6
jelly771001 <jelly771001@gmail.com>
2024-01-16 10:00:48 +0800

    fix: _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import wrapper from "@/redux/store";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps,
+  router,
+  ...rest
+}: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
-
-export default wrapper.withRedux(MyApp);
