00b7ae2242ba55df7ad1b005a55dcfa74f670476
jelly771001 <jelly771001@gmail.com>
2024-01-29 13:43:27 +0800

    feat: Add Noto Sans TC font and update layout styles

--- src/common/components/Layout/index.tsx
+++ src/common/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 
 
-import { LayoutPropsType, colorWhiteSet, footerSet } from './data';
+import { LayoutPropsType, colorWhiteSet, footerSet, notoTC } from './data';
 import CustomHead from '../CustomHead';
 import AuthLayout from './AuthLayout';
 import Header from '@/common/components/Header';
@@ -9,10 +9,11 @@ import FixedHeader from '../Header/FixedHeader';
 import ScrollPageTop from '../ScrollPageTop';
 import ContactService from '@/modules/ContactService';
 
+
 const Layout = ({ children, pageCategory, classStyle }: LayoutPropsType) => {
   return (
     <div
-      className={`${colorWhiteSet[pageCategory]} flex flex-col min-h-screen`}>
+      className={`${colorWhiteSet[pageCategory]} ${notoTC.className} flex flex-col min-h-screen`}>
       <CustomHead pageCategory={pageCategory} />
       <Header pageCategory={pageCategory} />
       <FixedHeader pageCategory={pageCategory} />
