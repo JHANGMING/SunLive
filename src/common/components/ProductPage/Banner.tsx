e263ea72ce52a62c9f31522564a155ff722fcea7
jelly771001 <jelly771001@gmail.com>
2024-01-29 09:25:12 +0800

    feat: Add Banner component to ProductPage

---
+++ src/common/components/ProductPage/Banner.tsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+  return (
+    <section>
+      <div className=" bg-commonBannerBG h-459 bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center"></div>
+      {/* <div className=" bg-shopBannerBG_2rd h-[357px] bg-repeat-x bg-center bg-contain -mt-[216px] flex justify-center items-end"></div> */}
+    </section>
+  );
+};
+
+export default Banner;
