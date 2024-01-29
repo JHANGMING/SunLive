f28c4e346235db419c3873c58780e54f5ab02bc7
jelly771001 <jelly771001@gmail.com>
2024-01-29 09:31:52 +0800

    Refactor: Pagination component and import order in ProductPage

--- src/modules/ProductPage/index.tsx
+++ src/modules/ProductPage/index.tsx
@@ -1,9 +1,10 @@
-import Banner from '@/common/components/ProductPage/Banner';
+
 import CategorySection from './CategorySection';
 import DiscountedSection from './DiscountedSection';
 import PopularSection from './PopularSection';
 import SeasonalSection from './SeasonalSection';
 import AllProductSection from './AllProductSection';
+import Banner from '@/common/components/ProductPage/Banner';
 
 const ProductPage = () => {
   return (
