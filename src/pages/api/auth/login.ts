b6ecffe10a4c8c42f5f7a27a88225e608b2eb9d9
jelly771001 <jelly771001@gmail.com>
2024-01-30 17:02:58 +0800

    feat: Add new CSS files and API endpoints

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ error: string }>
+) {
+  try {
+    const { email, password } = JSON.parse(req.body);
+    console.log(email, password);
+
+    const result = await fetch('http://4.224.41.94/api/login/general', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ Account: email, Password: password }),
+    });
+    const data = await result.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('API Error:', error);
+
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
