0c30ba0b7ca20d4593114f5714fad50f6cde22af
jelly771001 <jelly771001@gmail.com>
2024-02-04 09:56:18 +0800

    feat: Add productData and productAllPrice to CartPage

--- src/modules/CartPage/CartListSection.tsx
+++ src/modules/CartPage/CartListSection.tsx
@@ -1,11 +1,15 @@
 import LogoImg from '@/common/components/Logo/LogoImg';
 import Image from 'next/image';
 import { useState } from 'react';
-import { BsChevronRight, BsChevronDown, BsX } from 'react-icons/bs';
+import { BsChevronDown, BsX } from 'react-icons/bs';
 import CartLink from './CartLink';
 import SpecSelect from '@/common/components/Select/SpecSelect';
 import { generateSpecData } from '@/common/components/Select/SpecSelect/data';
+import { productData } from './data';
 const CartListSection = () => {
+  const handlerQtyChange = (id: number, delta: number) => {
+    console.log(id, delta);
+  };
   return (
     <section className="container">
       <div className=" flex gap-40">
@@ -19,154 +23,66 @@ const CartListSection = () => {
           </div>
           <ul
             className="cartlist form-transition">
-            <li className="p-24 flex justify-between">
-              <div className="flex gap-16">
-                <Image
-                  src="/images/home/live/liveComingImg1.png"
-                  alt="liveComingImg1"
-                  width={80}
-                  height={80}
-                  className="w-80 h-80"
-                />
-                <div className="">
-                  <h6 className=" font-normal">夢幻柳橙夏悠</h6>
-                  <div className="text-14 flex gap-8 items-center">
-                    <p>
-                      NT$<span>250</span>
-                    </p>
-                    <p className=" text-lightGray line-through">299</p>
-                  </div>
-                </div>
-              </div>
-              <SpecSelect optionsData={generateSpecData({smallWeight: 120,
-  largeWeight: 300})}/>
-              <div>
-                <div className="flex gap-x-12 items-center">
-                  <Image
-                    src="/images/cart/dec.png"
-                    alt="dec"
-                    width={20}
-                    height={20}
-                    className="w-20 h-20 cursor-pointer hover:opacity-70"
-                  />
-                  <p className="text-18">1</p>
-                  <Image
-                    src="/images/cart/plus.png"
-                    alt="plus"
-                    width={20}
-                    height={20}
-                    className="w-20 h-20 cursor-pointer hover:opacity-70"
-                  />
-                </div>
-              </div>
-              <div className=" flex gap-40">
-                <h6 className=" font-normal">
-                  <span>$</span>250
-                </h6>
-                <BsX
-                  size={24}
-                  className=" text-darkGray cursor-pointer hover:opacity-70"
-                />
-              </div>
-            </li>
-            <li className="p-24 flex justify-between">
-              <div className="flex gap-16">
-                <Image
-                  src="/images/home/live/liveComingImg1.png"
-                  alt="liveComingImg1"
-                  width={80}
-                  height={80}
-                  className="w-80 h-80"
-                />
-                <div className="">
-                  <h6 className=" font-normal">夢幻柳橙夏悠</h6>
-                  <div className="text-14 flex gap-8">
-                    <p>
-                      NT$<span>250</span>
-                    </p>
-                    <p className=" text-lightGray line-through">299</p>
-                  </div>
-                </div>
-              </div>
-              <div className="text-18">小份 (213g)</div>
-              <div>
-                <div className="flex gap-x-12 items-center">
-                  <Image
-                    src="/images/cart/dec.png"
-                    alt="dec"
-                    width={20}
-                    height={20}
-                    className="w-20 h-20 cursor-pointer hover:opacity-70"
-                  />
-                  <p className="text-18">1</p>
-                  <Image
-                    src="/images/cart/plus.png"
-                    alt="plus"
-                    width={20}
-                    height={20}
-                    className="w-20 h-20 cursor-pointer hover:opacity-70"
-                  />
-                </div>
-              </div>
-              <div className=" flex gap-40">
-                <h6 className=" font-normal">
-                  <span>$</span>250
-                </h6>
-                <BsX
-                  size={24}
-                  className=" text-darkGray cursor-pointer hover:opacity-70"
-                />
-              </div>
-            </li>
-            <li className="p-24 flex justify-between">
-              <div className="flex gap-16">
-                <Image
-                  src="/images/home/live/liveComingImg1.png"
-                  alt="liveComingImg1"
-                  width={80}
-                  height={80}
-                  className="w-80 h-80"
-                />
-                <div className="">
-                  <h6 className=" font-normal">夢幻柳橙夏悠</h6>
-                  <div className="text-14 flex gap-8">
-                    <p>
-                      NT$<span>250</span>
-                    </p>
-                    <p className=" text-lightGray line-through">299</p>
-                  </div>
-                </div>
-              </div>
-              <div className="text-18">小份 (213g)</div>
-              <div>
-                <div className="flex gap-x-12 items-center">
-                  <Image
-                    src="/images/cart/dec.png"
-                    alt="dec"
-                    width={20}
-                    height={20}
-                    className="w-20 h-20 cursor-pointer hover:opacity-70"
-                  />
-                  <p className="text-18">1</p>
-                  <Image
-                    src="/images/cart/plus.png"
-                    alt="plus"
-                    width={20}
-                    height={20}
-                    className="w-20 h-20 cursor-pointer hover:opacity-70"
-                  />
-                </div>
-              </div>
-              <div className=" flex gap-40">
-                <h6 className=" font-normal">
-                  <span>$</span>250
-                </h6>
-                <BsX
-                  size={24}
-                  className=" text-darkGray cursor-pointer hover:opacity-70"
-                />
-              </div>
-            </li>
+              {productData.map((data) => {
+                const {productID,productImg,productTitle,smallOriginalPrice,smallPromotionPrice,spec,qyt,total }=data
+                return (
+                  <li key={productID} className="p-24 flex gap-60">
+                    <div className="flex gap-16 flex-grow">
+                      <Image
+                        src={productImg.src}
+                        alt={productImg.alt}
+                        width={80}
+                        height={80}
+                        className="w-80 h-80"
+                      />
+                      <div>
+                        <h6 className=" font-normal">{productTitle}</h6>
+                        <div className="text-14 flex gap-8 items-center">
+                          <p>
+                            NT$<span>{smallPromotionPrice}</span>
+                          </p>
+                          <p className=" text-lightGray line-through">
+                            {smallOriginalPrice}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <SpecSelect optionsData={generateSpecData(spec)} />
+                    <div>
+                      <div className="flex gap-x-12 items-center">
+                        <Image
+                          src="/images/cart/dec.png"
+                          alt="dec"
+                          width={20}
+                          height={20}
+                          className="w-20 h-20 cursor-pointer hover:opacity-70"
+                          onClick={() => handlerQtyChange(productID, qyt - 1)}
+                        />
+                        <p className="text-18">{qyt}</p>
+                        <Image
+                          src="/images/cart/plus.png"
+                          alt="plus"
+                          width={20}
+                          height={20}
+                          className="w-20 h-20 cursor-pointer hover:opacity-70"
+                          onClick={() => handlerQtyChange(productID, qyt + 1)}
+                        />
+                      </div>
+                    </div>
+                    <div className=" flex gap-40">
+                      <h6 className=" font-normal">
+                        <span>$</span>
+                        {total}
+                      </h6>
+                      <BsX
+                        size={24}
+                        className=" text-darkGray cursor-pointer hover:opacity-70"
+                      />
+                    </div>
+                  </li>
+                );
+              }
+              )}
           </ul>
         </div>
         <div className="w-3/12">
@@ -193,7 +109,7 @@ const CartListSection = () => {
               <div className="flex">
                 <div className="text-18 text-primary-red w-1/2">折扣</div>
                 <p className="text-20 text-primary-red font-bold w-1/2">
-                  $<span>650</span>
+                  $<span>150</span>
                 </p>
               </div>
             </div>
