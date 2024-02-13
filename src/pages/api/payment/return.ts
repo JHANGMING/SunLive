f9764373ca6c4a390fc7786f7a85dcf788cabea4
jelly771001 <jelly771001@gmail.com>
2024-02-13 21:20:34 +0800

    Add FailOrder and SuccessOrder components for payment pages

--- src/pages/api/payment/return.ts
+++ src/pages/api/payment/return.ts
@@ -4,9 +4,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const status = req.body.Status;
     if (status === 'SUCCESS') {
-      res.redirect(302, '/payment/success-order');
+      res.redirect(302, '/payment/success');
     } else {
-      res.redirect(302, '/payment/failure-order');
+      res.redirect(302, '/payment/fail');
     }
   } else {
     res.status(405).end();
---
+++ src/pages/payment/success.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/common/components/Layout';
+import SuccessOrder from '@/modules/CartPage/payment/SuccessOrder';
+
+const Order = () => {
+  return (
+    <Layout pageCategory="CartPage">
+      <SuccessOrder />
+    </Layout>
+  );
+};
+
+export default Order;
