39079d87174998aeca16cc19d3343b59224332ac
jelly771001 <jelly771001@gmail.com>
2024-02-27 15:45:44 +0800

    Refactor DashboardLayout component

--- src/common/components/Layout/DashboardLayout.tsx
+++ src/common/components/Layout/DashboardLayout.tsx
@@ -1,18 +1,24 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import useAuth from "@/common/hooks/useAuth";
 import DashboardPage from "@/modules/DashboardPage";
 import { DashboardLayoutProps } from "./data";
-import useAuth from "@/common/hooks/useAuth";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Loading from "../Loading/Loading";
 
 const DashboardLayout = ({ children }:DashboardLayoutProps) => {
-  // const auth = useAuth();
-  //  const router = useRouter();
-  // useEffect(() => {
-  //   if (auth?.category !== '1') {
-  //     router.replace('/404');
-  //   }
-  // }, [auth]);
-  // if (auth?.category !== '1') return null;
+  const [isClient, setIsClient] = useState(false);
+  const auth = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    setIsClient(true);
+    if (auth?.category !== '1') {
+      router.replace('/404');
+    }
+  }, [auth]);
+  if (!isClient) {
+    return <Loading/>;
+  }
+  if (auth?.category !== '1') return ;
   return (
     <section className="pt-60 pb-[194px] container flex gap-[74px]">
       <DashboardPage />
