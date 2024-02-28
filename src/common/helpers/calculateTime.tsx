c5577e51d5ed7fe79e712f0b6f9c63501bcba9cd
jelly771001 <jelly771001@gmail.com>
2024-02-28 21:25:05 +0800

    Add calculateTimeLeft helper function

---
+++ src/common/helpers/calculateTime.tsx
@@ -0,0 +1,33 @@
+export const calculateTimeLeft = (endTime:string) => {
+  if (typeof endTime !== 'string' || !endTime) {
+    return { hours: '00', minutes: '00', seconds: '00' };
+  }
+  const endTimeParts = endTime.split(':').map((num) => parseInt(num, 10));
+  if (
+    endTimeParts.length < 2 ||
+    isNaN(endTimeParts[0]) ||
+    isNaN(endTimeParts[1])
+  ) {
+    return { hours: '00', minutes: '00', seconds: '00' };
+  }
+  const now = new Date();
+  const endDate = new Date(now);
+  endDate.setHours(endTimeParts[0], endTimeParts[1], 0, 0);
+
+  const timeDiff = endDate.getTime() - now.getTime();
+  if (timeDiff <= 0) {
+    return { hours: '00', minutes: '00', seconds: '00' };
+  }
+
+  return {
+    hours: Math.floor((timeDiff / (1000 * 60 * 60)) % 24)
+      .toString()
+      .padStart(2, '0'),
+    minutes: Math.floor((timeDiff / (1000 * 60)) % 60)
+      .toString()
+      .padStart(2, '0'),
+    seconds: Math.floor((timeDiff / 1000) % 60)
+      .toString()
+      .padStart(2, '0'),
+  };
+};
