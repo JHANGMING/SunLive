cb3e88e86badf1befb18c8bb2573118bc97224d9
jelly771001 <jelly771001@gmail.com>
2024-02-29 14:55:16 +0800

    Refactor Toast component

--- src/common/components/Toast/index.tsx
+++ src/common/components/Toast/index.tsx
@@ -1,22 +1,24 @@
-import { useEffect} from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
-import { hideToast } from "@/redux/features/messageSlice";
-import LogoImg from "../Logo/LogoImg";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { hideToast } from '@/redux/features/messageSlice';
+import LogoImg from '../Logo/LogoImg';
 
 const Toast = () => {
- const dispatch = useDispatch();
- const { showMessage, message } = useSelector((state:RootState) => state.message);
- useEffect(() => {
-   if (showMessage) {
-     const timer = setTimeout(() => {
-       dispatch(hideToast());
-     }, 3000);
-     return () => clearTimeout(timer);
-   }
- }, [showMessage, dispatch]);
+  const dispatch = useDispatch();
+  const { showMessage, message } = useSelector(
+    (state: RootState) => state.message
+  );
+  useEffect(() => {
+    if (showMessage) {
+      const timer = setTimeout(() => {
+        dispatch(hideToast());
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showMessage, dispatch]);
 
- if (!showMessage) return null;
+  if (!showMessage) return null;
 
   return (
     <div className="fixed top-0 right-0 flex gap-8 bg-primary-yellow rounded-tl-8 rounded-bl-8 p-12 z-50">
