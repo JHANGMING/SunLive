b85b9575499664bc184a68508c57a9c2fd05ee49
jelly771001 <jelly771001@gmail.com>
2024-02-29 14:54:27 +0800

    Refactor Logo component

--- src/common/components/Logo/index.tsx
+++ src/common/components/Logo/index.tsx
@@ -3,14 +3,15 @@ import Image from '@/common/components/CustomImage';
 import LogoImg from './LogoImg';
 import { classStyleProps } from './data';
 
-const Logo = ({
-  classStyle,
-  classProps,
-}: classStyleProps) => {
+const Logo = ({ classStyle, classProps }: classStyleProps) => {
   return (
     <Link href="/" className={`flex items-center gap-16 group ${classStyle}`}>
       <LogoImg classProps={`group-shake ${classProps}`} />
-      <Image src="/images/sunlive_logo.svg" alt="sunLive-logo" className='w-100 h-[42px]'/>
+      <Image
+        src="/images/sunlive_logo.svg"
+        alt="sunLive-logo"
+        className="w-100 h-[42px]"
+      />
     </Link>
   );
 };
