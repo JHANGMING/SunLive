0fea668af4143f4e054bf70da1839dc9b1c600dd
jelly771001 <jelly771001@gmail.com>
2024-02-29 12:12:39 +0800

    Remove unused import in account.tsx

--- src/pages/dashboard/account.tsx
+++ src/pages/dashboard/account.tsx
@@ -1,5 +1,4 @@
 import Layout from '@/common/components/Layout';
-import DashboardPage from '@/modules/DashboardPage';
 import AccountSettng from '@/modules/DashboardPage/AccountSettng';
 const Account = () => {
   return (
