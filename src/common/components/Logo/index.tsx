1ba902901722f057576b61696f16b5c1e1040eeb
jelly771001 <jelly771001@gmail.com>
2024-02-29 13:38:26 +0800

    Remove commented out code in Logo component

--- src/common/components/Logo/index.tsx
+++ src/common/components/Logo/index.tsx
@@ -6,13 +6,11 @@ import { classStyleProps } from './data';
 const Logo = ({
   classStyle,
   classProps,
-  textSytle = 'text-28',
 }: classStyleProps) => {
   return (
     <Link href="/" className={`flex items-center gap-16 group ${classStyle}`}>
       <LogoImg classProps={`group-shake ${classProps}`} />
       <Image src="/images/sunlive_logo.svg" alt="sunLive-logo" className='w-100 h-[42px]'/>
-      {/* <p className={`${textSytle} font-bold text-primary-green`}>搶鮮購</p> */}
     </Link>
   );
 };
