a09f9980c160b5f2d06c8fed38538ac32ebf64b8
jelly771001 <jelly771001@gmail.com>
2024-03-01 11:50:53 +0800

    Refactor liveSettings.tsx: Update LiveproudctTpye and EditSettingType structures

--- src/constants/types/dashboard/liveSettings.tsx
+++ src/constants/types/dashboard/liveSettings.tsx
@@ -1,17 +1,17 @@
 type LiveproudctTpye = {
   productId: number; //本頁面不設定置頂產品，後端採用第一筆預設為置頂產品
-  productSize: boolean; // 小為false、大為 true
   liveprice: number;
+  productSize: boolean; // 小為false、大為 true
 };
 
 export type EditSettingType = {
+  yturl: string;
   liveId: number;
+  endTime: string;
+  livepic: string;
   liveName: string;
   liveDate: string;
   startTime: string;
-  endTime: string;
-  livepic: string;
-  yturl: string;
   topProductId: number;
   topProductSize: boolean;
   liveproudct: LiveproudctTpye[];
