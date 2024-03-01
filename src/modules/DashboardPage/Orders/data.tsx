b23f67ffba020ce68b15b36e42deb3896171445c
jelly771001 <jelly771001@gmail.com>
2024-03-01 11:51:41 +0800

    Update order tabs in OrdersDashboardProps

--- src/modules/DashboardPage/Orders/data.tsx
+++ src/modules/DashboardPage/Orders/data.tsx
@@ -6,6 +6,6 @@ export type OrdersDashboardProps = {
 
 export const tabs = [
   { name: '所有訂單', id: 'allorders' },
-  { name: '未出貨訂單', id: 'unshippedorders' },
   { name: '已出貨訂單', id: 'shippedorders' },
+  { name: '未出貨訂單', id: 'unshippedorders' },
 ];
