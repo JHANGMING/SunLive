d64e09f82953aac6acfec4759cac71ac0f3b2a8e
jelly771001 <jelly771001@gmail.com>
2024-03-01 11:49:46 +0800

    Refactor GenderSelect component props
    This commit refactors the GenderSelect component props by moving the `control` prop to the bottom and adding a `defaultValue` prop.

--- src/common/components/Select/GenderSelect/data.tsx
+++ src/common/components/Select/GenderSelect/data.tsx
@@ -2,10 +2,10 @@ import { Control } from 'react-hook-form';
 import { FormValues } from '../../Input/data';
 
 export type GenderSelectProps = {
-  control: Control<FormValues>;
   labelText: string;
   id: keyof FormValues;
   defaultValue?: OptionType;
+  control: Control<FormValues>;
 };
 export type OptionType = {
   value: string;
