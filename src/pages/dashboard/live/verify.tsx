b34368fc5d35c7f844bb9dba4644b84a7b101c96
jelly771001 <jelly771001@gmail.com>
2024-03-13 17:01:23 +0800

    Refactor verify.tsx file

--- src/pages/dashboard/live/verify.tsx
+++ src/pages/dashboard/live/verify.tsx
@@ -1,27 +1,20 @@
 import { useRouter } from 'next/router';
 import { useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { nextRoutes } from '@/constants/apiPaths';
+import { nextRoutes } from '@/constants/api/apiPaths';
 import Loading from '@/common/components/Loading/Loading';
 import { showLoading } from '@/redux/features/messageSlice';
-import fetchNextApi, { apiParamsType } from '@/common/helpers/fetchNextApi';
+import fetchNextApi, { NextapiParamsType } from '@/common/helpers/fetchNextApi';
+
 const Verify = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const [token, setToken] = useState('');
-  useEffect(() => {
-    dispatch(showLoading());
-    const { code } = router.query;
-    if (code) {
-      handerVerify(code as string);
-    }
-  }, [router, token]);
-
   const handerVerify = async (code: string) => {
-    const apiParams: apiParamsType = {
-      apiPath: nextRoutes['verify'],
+    const apiParams: NextapiParamsType = {
+      apiPath: nextRoutes.verify,
       method: 'POST',
-      data: { code: code },
+      data: { code },
     };
     try {
       const result = await fetchNextApi(apiParams);
@@ -30,7 +23,7 @@ const Verify = () => {
         if (window.opener) {
           window.opener.postMessage(
             { type: 'auth', token: result.token },
-            'https://sun-live.vercel.app'
+            'https://sun-live.vercel.app',
           );
           window.close();
         } else {
@@ -41,6 +34,14 @@ const Verify = () => {
       console.error('Error fetching user data:', error);
     }
   };
+  useEffect(() => {
+    dispatch(showLoading());
+    const { code } = router.query;
+    if (code) {
+      handerVerify(code as string);
+    }
+  }, [router, token]);
+
   return <Loading />;
 };
 
