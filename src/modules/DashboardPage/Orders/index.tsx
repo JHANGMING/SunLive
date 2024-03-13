29a6bcd54bb19a8e9dc773da3a679a83ce069198
jelly771001 <jelly771001@gmail.com>
2024-03-13 16:50:53 +0800

    Refactor order tabs to use buttons instead of h4 tags

--- src/modules/DashboardPage/Orders/index.tsx
+++ src/modules/DashboardPage/Orders/index.tsx
@@ -9,14 +9,16 @@ const OrdersDashboard = ({ children }: OrdersDashboardProps) => {
     <div className="w-9/12 flex-grow">
       <div className=" bg-white rounded-20 py-18 pl-28 flex gap-24 mb-40">
         {tabs.map((tab) => (
-          <h4
+          <button
+            type="button"
             key={tab.id}
             onClick={() => router.push(`/dashboard/orders/${tab.id}`)}
             className={`text-16 font-normal py-10 px-8 rounded-8 cursor-pointer hover:opacity-70 ${
               orderId === tab.id ? 'bg-primary-yellow ' : ''
-            }`}>
+            }`}
+          >
             {tab.name}
-          </h4>
+          </button>
         ))}
       </div>
       {children}
