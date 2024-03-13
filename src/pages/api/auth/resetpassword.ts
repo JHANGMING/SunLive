24fa6a11976535664ced226aeb7bf57bc66ad07b
jelly771001 <jelly771001@gmail.com>
2024-03-13 16:55:07 +0800

    Update import path for apiPaths constant

--- src/pages/api/auth/resetpassword.ts
+++ src/pages/api/auth/resetpassword.ts
@@ -1,14 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { apiPaths } from '@/constants/apiPaths';
+import { apiPaths } from '@/constants/api/apiPaths';
 import fetchApi, { ApiParamsType } from '@/common/helpers/fetchApi';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ error: string }>
+  res: NextApiResponse<{ error: string }>,
 ) {
   try {
     const apiParams: ApiParamsType = {
-      apiPath: apiPaths['resetpassword'],
+      apiPath: apiPaths.resetpassword,
       method: 'POST',
       data: req.body,
     };
