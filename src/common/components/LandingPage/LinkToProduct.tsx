fd91aa29c3db802fb7aa0dfc6ff162b552a933dd
jelly771001 <jelly771001@gmail.com>
2024-03-13 16:21:53 +0800

    Update LinkToProduct component styling

--- src/common/components/LandingPage/LinkToProduct.tsx
+++ src/common/components/LandingPage/LinkToProduct.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { BsHandIndex } from 'react-icons/bs';
+
 type LinkToProductProps = {
   path: string;
   text: string;
@@ -8,7 +9,8 @@ const LinkToProduct = ({ path, text }: LinkToProductProps) => {
   return (
     <Link
       href={path}
-      className="group flex items-center justify-end gap-8 self-start col-start-11 col-end-13 mb-24 lg:mb-40">
+      className="group flex items-center justify-end gap-8 self-start col-start-11 col-end-13 mb-24 lg:mb-40"
+    >
       <p className="text-14 lg:text-16 font-bold">{text}</p>
       <BsHandIndex className="rotate-90 w-[32px] h-[32px] lg:w-44 lg:h-44 bg-primary-red text-white px-10 rounded-full group-hover:bg-primary-yellow" />
     </Link>
