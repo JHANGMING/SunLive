df7a783d1e3656b8a55e47f4fa6b2f1c0bbcecac
jelly771001 <jelly771001@gmail.com>
2024-03-13 16:52:02 +0800

    Update import paths and fix linting issues

--- src/modules/PersonInfoPage/AllOrders.tsx
+++ src/modules/PersonInfoPage/AllOrders.tsx
@@ -1,15 +1,15 @@
 import useSWR from 'swr';
-import { nextRoutes } from '@/constants/apiPaths';
-import { fetcher } from '@/common/helpers/fetcher';
-import { useAuthStatus } from '@/common/hooks/useAuthStatus';
+import { nextRoutes } from '@/constants/api/apiPaths';
+import fetcher from '@/common/helpers/fetcher';
+import useAuthStatus from '@/common/hooks/useAuthStatus';
 import DynamicTable from '@/common/components/DynamicTable/userOrderTable';
 import { ordersColumns } from './data';
 
 const AllOrders = () => {
   const { authStatus } = useAuthStatus();
   const { data } = useSWR(
-    authStatus ? `/api${nextRoutes['orderlist']}` : null,
-    fetcher
+    authStatus ? `/api${nextRoutes.orderlist}` : null,
+    fetcher,
   );
   return (
     <>
