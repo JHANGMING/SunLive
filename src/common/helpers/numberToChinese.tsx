d39bb00458bec27bafe6d78c6fb701a22c6e712c
jelly771001 <jelly771001@gmail.com>
2024-03-13 16:44:14 +0800

    Refactor numberToChinese function

--- src/common/helpers/numberToChinese.tsx
+++ src/common/helpers/numberToChinese.tsx
@@ -1,4 +1,5 @@
-export const numberToChinese = (number: number) => {
+const numberToChinese = (number: number) => {
   const chineseNumbers = ['零', '一', '二', '三'];
   return chineseNumbers[number] || number;
 };
+export default numberToChinese;
