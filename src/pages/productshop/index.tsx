ab23e2528164fa3f380cab202fee3053ae0579b9
jelly771001 <jelly771001@gmail.com>
2024-03-13 17:02:23 +0800

    Update import paths for API constants

--- src/pages/productshop/index.tsx
+++ src/pages/productshop/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ProductPage from '@/modules/ProductPage';
-import { apiPaths } from '@/constants/apiPaths';
+import { apiPaths } from '@/constants/api/apiPaths';
 import Layout from '@/common/components/Layout';
 import { ProductShopProps } from '@/modules/ProductPage/data';
 import { setAllProductsData } from '@/redux/features/productSlice';
 import fetchApi, { ApiParamsType } from '@/common/helpers/fetchApi';
-import { ProductsRefProvider } from '@/common/hooks/ProductsRefContext';
+import { ProductsRefProvider } from '@/common/components/product/ProductsRefContext';
 
 const ProductShop = ({
   fruitProduct,
@@ -24,7 +24,7 @@ const ProductShop = ({
         allproductsData,
         promotionProduct,
         vegetableProduct,
-      })
+      }),
     );
   }, [
     fruitProduct,
@@ -52,36 +52,35 @@ export async function getServerSideProps() {
   let promotionProduct = [];
   let vegetableProduct = [];
 
-    // 取得所有商品
-    const allParams: ApiParamsType = {
-      apiPath: apiPaths['allproducts'],
-      method: 'GET',
-    };
+  // 取得所有商品
+  const allParams: ApiParamsType = {
+    apiPath: apiPaths.allproducts,
+    method: 'GET',
+  };
 
-    // 取得其他分類商品
-    const otherCategoryParams: ApiParamsType = {
-      apiPath: apiPaths['otherCategory'],
-      method: 'GET',
-    };
-    const responses = await Promise.allSettled([
-      fetchApi(allParams),
-      fetchApi(otherCategoryParams),
-    ]);
-    if (
-      responses[0].status === 'fulfilled' &&
-      responses[0].value.statusCode === 200
-    ) {
-      allproductsData = responses[0].value.data;
-    }
+  // 取得其他分類商品
+  const otherCategoryParams: ApiParamsType = {
+    apiPath: apiPaths.otherCategory,
+    method: 'GET',
+  };
+  const responses = await Promise.allSettled([
+    fetchApi(allParams),
+    fetchApi(otherCategoryParams),
+  ]);
+  if (
+    responses[0].status === 'fulfilled'
+    && responses[0].value.statusCode === 200
+  ) {
+    allproductsData = responses[0].value.data;
+  }
 
-    if (
-      responses[1].status === 'fulfilled' &&
-      responses[1].value.statusCode === 200
-    ) {
-      const { data } = responses[1].value;
-      ({ fruitProduct, topSaleProduct, promotionProduct, vegetableProduct } =
-        data);
-    }
+  if (
+    responses[1].status === 'fulfilled'
+    && responses[1].value.statusCode === 200
+  ) {
+    const { data } = responses[1].value;
+    ({ fruitProduct, topSaleProduct, promotionProduct, vegetableProduct } = data);
+  }
 
   return {
     props: {
