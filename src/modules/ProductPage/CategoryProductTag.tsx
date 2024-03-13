995ea3a6e260ca57e4546ca3c026b8ca02260b69
jelly771001 <jelly771001@gmail.com>
2024-03-13 16:52:39 +0800

    Fix formatting issue in CategoryProductTag component

--- src/modules/ProductPage/CategoryProductTag.tsx
+++ src/modules/ProductPage/CategoryProductTag.tsx
@@ -4,7 +4,8 @@ import { CategoryProductTagProps } from './data';
 const CategoryProductTag = ({ classStyle, text }: CategoryProductTagProps) => {
   return (
     <div
-      className={`w-220 h-74 rounded-20 bg-white flex gap-10 justify-center items-center   absolute z-10 ${classStyle}`}>
+      className={`w-220 h-74 rounded-20 bg-white flex gap-10 justify-center items-center   absolute z-10 ${classStyle}`}
+    >
       <LogoImg classProps="w-32 h-32" />
       <h3>{text}</h3>
     </div>
