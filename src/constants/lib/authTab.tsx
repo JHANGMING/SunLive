2289fe2c440d3e23718e3d8ea102dbc98d711676
jelly771001 <jelly771001@gmail.com>
2024-03-14 15:52:33 +0800

    Add AuthTabData interface and constants

---
+++ src/constants/lib/authTab.tsx
@@ -0,0 +1,14 @@
+interface AuthTabData {
+  welcome: string;
+  register: string;
+  noToken: string;
+}
+
+const authTab: AuthTabData = {
+  welcome: '歡迎回來',
+  register: '註冊成功',
+  noToken: '請重新登入',
+};
+
+export default authTab;
+
