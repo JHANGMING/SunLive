6d64ad3018bd8c3bee7c441e695e12eb0cb352fd
jelly771001 <jelly771001@gmail.com>
2024-03-19 13:57:44 +0800

    Remove unused code for setting cookie

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,4 +1,4 @@
-import { setCookie } from 'cookies-next';
+
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { apiPaths } from '@/constants/api/apiPaths';
 import fetchApi, { ApiParamsType } from '@/common/helpers/fetchApi';
@@ -16,15 +16,6 @@ export default async function handler(
       data: { account: email, password },
     };
     const result = await fetchApi(apiParams);
-    setCookie('token', result.token, {
-      req,
-      res,
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-      path: '/',
-      maxAge: 24 * 60 * 60,
-    });
     res.status(200).json(result);
   } catch (error) {
     console.error('API Error:', error);
