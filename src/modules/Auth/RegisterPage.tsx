6d16e0e964057d249a5b43f6d3df273cb1d7f8cd
jelly771001 <jelly771001@gmail.com>
2024-03-19 13:57:21 +0800

    Fix dispatch setToast message in RegisterPage

--- src/modules/Auth/RegisterPage.tsx
+++ src/modules/Auth/RegisterPage.tsx
@@ -43,7 +43,7 @@ const RegisterPage = () => {
         dispatch(
           setToast({
             message: authTabData.register,
-          })
+          }),
         );
       } else {
         dispatch(setToast({ message: `${result.message || '未知錯誤'}` }));
