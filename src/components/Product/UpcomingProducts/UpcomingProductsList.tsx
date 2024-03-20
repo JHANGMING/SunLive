cb0551e81d42481b0eb0f46660ee2c5e31d6f818
jelly771001 <jelly771001@gmail.com>
2024-03-20 11:53:58 +0800

    Add UpcomingProduct and UpcomingProductsList components

---
+++ src/components/Product/UpcomingProducts/UpcomingProductsList.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import updateUpcomingLiveWithStyles from '@/common/helpers/upcomingDataAddclass';
+import UpcomingProduct from './UpcomingProduct';
+
+const UpcomingProductsList = () => {
+  const { liveData } = useSelector((state: RootState) => state.product);
+  const upcomingLive = liveData?.data?.upcomingLive || [];
+  const updatedUpcomingLive = updateUpcomingLiveWithStyles(upcomingLive);
+  return (
+    <div className=" col-start-5 col-end-11">
+      <ul className="flex flex-col gap-40">
+        {updatedUpcomingLive.map((data) => (
+          <UpcomingProduct key={data.liveId} {...data} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UpcomingProductsList;
