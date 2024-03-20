a8ee339fae097290776828a3274636d92eb6a737
jelly771001 <jelly771001@gmail.com>
2024-03-20 11:07:30 +0800

    Refactor DeleteBtn component to use updated API parameters

--- src/common/components/Button/DeleteBtn.tsx
+++ src/common/components/Button/DeleteBtn.tsx
@@ -1,22 +1,16 @@
 import { mutate } from 'swr';
 import { BsX } from 'react-icons/bs';
 import { useDispatch } from 'react-redux';
-import { nextRoutes } from '@/constants/api/apiPaths';
 import { setToast } from '@/redux/features/messageSlice';
 import useDebounceFn from '@/common/hooks/useDebounceFn';
-import fetchNextApi, { NextapiParamsType } from '@/common/helpers/fetchNextApi';
+import fetchNextApi from '@/common/helpers/fetchNextApi';
+import { deletecartParams } from '@/constants/api/nextApiParams';
 import { DeleteBtnPropsType } from './data';
 
 const DeleteBtn = ({ size, className, productSpecId }: DeleteBtnPropsType) => {
   const dispatch = useDispatch();
   const handlerDeleteItem = useDebounceFn(async () => {
-    const apiParams: NextapiParamsType = {
-      apiPath: nextRoutes.deletecart,
-      method: 'POST',
-      data: {
-        productSpecId,
-      },
-    };
+    const apiParams = { ...deletecartParams, data: { productSpecId } };
     try {
       const result = await fetchNextApi(apiParams);
       if (result.statusCode === 200) {
