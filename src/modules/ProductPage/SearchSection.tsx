aaec637be5f6a7799955b77cd6f5e90d15ff12b5
jelly771001 <jelly771001@gmail.com>
2024-03-26 16:34:41 +0800

    Add SearchSection component to ProductPage

---
+++ src/modules/ProductPage/SearchSection.tsx
@@ -0,0 +1,61 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { RootState } from '@/redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import SearchInput from '@/components/Input/SearchInput';
+import ProductList from '@/components/Product/ProductList';
+import { showLoading } from '@/redux/features/messageSlice';
+import { setSearchData } from '@/redux/features/productSlice';
+import CategoryTitle from '@/modules/ProductPage/CategoryTitle';
+
+const SearchSection = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { searchData, searchTag } = useSelector(
+    (state: RootState) => state.product,
+  );
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (!url.includes('/search')) {
+        dispatch(setSearchData({ searchTag: '輸入水果、蔬菜' }));
+        dispatch(showLoading());
+      }
+    };
+    router.events.on('routeChangeStart', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [router]);
+  return (
+    <section className="bg-searchBannerBG bg-repeat-x -mt-[216px] pt-120  ">
+      <div className="bg-lightWhite pb-60">
+        <div className="container ">
+          <div className="flex items-center">
+            <CategoryTitle title="搜尋結果" gapStyle="mb-8" />
+            <SearchInput headerVisible />
+          </div>
+          <h4 className="mb-24">
+            {searchTag}
+            的搜尋結果共
+            {' '}
+            <span className=" text-primary-green">{searchData?.length}</span>
+            {' '}
+            筆
+          </h4>
+          {searchData?.length > 0 ? (
+            <ProductList category="search" />
+          ) : (
+            <p>
+              不好意思！
+              <br />
+              我們找不到相關的商品，請輸入其他字詞或探索以下農產品
+            </p>
+          )}
+          {/* */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SearchSection;
