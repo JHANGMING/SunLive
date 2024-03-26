20e89f608b863d8be2ebd38cb984563a170e05ae
jelly771001 <jelly771001@gmail.com>
2024-03-26 13:58:12 +0800

    Add banner image data and title constants

---
+++ src/components/Banner/LandingPageBanner/data.tsx
@@ -0,0 +1,38 @@
+export const bannerImgData = [
+  {
+    src: '/images/home/banner/banner1.png',
+    alt: 'banner1',
+  },
+  {
+    src: '/images/home/banner/banner2.png',
+    alt: 'banner2',
+  },
+  {
+    src: '/images/home/banner/banner3.png',
+    alt: 'banner3',
+  },
+  {
+    src: '/images/home/banner/banner4.png',
+    alt: 'banner4',
+  },
+  {
+    src: '/images/home/banner/banner5.png',
+    alt: 'banner5',
+  },
+];
+
+export const bannerTitle = {
+  title: '對土地保持友善的態度',
+  subTitle: '為你提供最安心、最放心的農產品',
+};
+
+export const grassMotionSet = {
+  grassMotionLeft: {
+    src: '/images/grassMotionLeft.png',
+    alt: 'grassMotionLeft',
+  },
+  grassMotionRight: {
+    src: '/images/grassMotionRight.png',
+    alt: 'grassMotionRight',
+  },
+};
