9f971826fd24f5a1b995db0b9c3c52046f7a0deb
jelly771001 <jelly771001@gmail.com>
2024-03-26 14:01:38 +0800

    Delete Banner component

--- src/components/ProductPage/Banner.tsx
+++
@@ -1,9 +0,0 @@
-const Banner = () => {
-  return (
-    <section>
-      <div className=" bg-commonBannerBG h-459 bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center" />
-    </section>
-  );
-};
-
-export default Banner;
