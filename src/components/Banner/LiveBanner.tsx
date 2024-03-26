25be266280195767cc5c4cd093ee9db41c17c323
jelly771001 <jelly771001@gmail.com>
2024-03-26 13:57:45 +0800

    Add LiveBanner component

---
+++ src/components/Banner/LiveBanner.tsx
@@ -0,0 +1,10 @@
+const LiveBanner = () => {
+  return (
+    <section>
+      <div className=" bg-commonBannerBG h-459 bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center" />
+      <div className=" bg-liveBannerBG h-[357px] bg-repeat-x bg-center -mt-[216px] flex justify-center items-end" />
+    </section>
+  );
+};
+
+export default LiveBanner;
