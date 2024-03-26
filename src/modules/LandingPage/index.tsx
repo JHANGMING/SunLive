c012d6d2a8d773b4d0ffe3c652e7db36d3bf73e3
jelly771001 <jelly771001@gmail.com>
2024-03-26 14:04:17 +0800

    Update import paths for LandingPage components

--- src/modules/LandingPage/index.tsx
+++ src/modules/LandingPage/index.tsx
@@ -1,11 +1,11 @@
 import HomeSwiper from '@/components/HomeSwiper';
-import Banner from '@/components/LandingPage/banner';
+import Banner from '@/components/Banner/LandingPageBanner';
 import LivingShowSection from '@/modules/LandingPage/LivingShowSection';
 import ProductListSection from '@/modules/LandingPage/ProductListSection';
 import RecommendSection from '@/components/LandingPage/RecommendSection';
 import ComingProductsSection from '@/modules/LandingPage/ComingProductsSection';
-import FarmFeatureSection from '@/components/LandingPage/farmFeatureSection';
-import FriendlyFarmSection from '@/components/LandingPage/friendlyFarmSection';
+import FarmFeatureSection from '@/components/LandingPage/FarmFeature';
+import FriendlyFarmSection from '@/components/LandingPage/FriendlyFarm';
 
 const LandingPage = () => {
   return (
