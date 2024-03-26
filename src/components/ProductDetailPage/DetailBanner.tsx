f17811193d8b01fe261161e7fe20dacccdae583d
jelly771001 <jelly771001@gmail.com>
2024-03-26 14:01:32 +0800

    Delete DetailBanner component

--- src/components/ProductDetailPage/DetailBanner.tsx
+++
@@ -1,9 +0,0 @@
-const DetailBanner = () => {
-  return (
-    <section>
-      <div className=" bg-commonBannerBG h-[216px] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center" />
-    </section>
-  );
-};
-
-export default DetailBanner;
