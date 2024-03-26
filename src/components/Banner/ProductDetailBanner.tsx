c39abb880544cc528af26a57d8c461652c37b01c
jelly771001 <jelly771001@gmail.com>
2024-03-26 13:57:51 +0800

    Add ProductDetailBanner component

---
+++ src/components/Banner/ProductDetailBanner.tsx
@@ -0,0 +1,9 @@
+const ProductDetailBanner = () => {
+  return (
+    <section>
+      <div className=" bg-commonBannerBG h-[216px] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center" />
+    </section>
+  );
+};
+
+export default ProductDetailBanner;
